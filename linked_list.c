09ab22924fa94293a6a6322f6ac7862154b353f7
0418yicheng <0418yicheng@gmail.com>
2025-04-04 16:24:52 -0400

    Finished hw10

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,70 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+void free_util(struct list_node* node);
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+struct list_node *new_node(size_t value) {
+    struct list_node* node = malloc(sizeof(struct list_node));;
+    node->value = value;
+    return node; 
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+    struct list_node* node = new_node(value);
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+    node->next = list->head;
+    list->head = node;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+    struct list_node* node = list->head;
+    while(node->next != NULL){
+	node = node->next;
+    }
 
-void free_list(struct linked_list list) {}
+    node->next = new_node(value);
+}
+
+size_t remove_from_head(struct linked_list *list) {
+    struct list_node* delete = list->head;
+    size_t value = list->head->value;
+
+    list->head = list->head->next;
+
+    free(delete);
+
+    return value; 
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+    struct list_node* node = list->head;
+    struct list_node* prev;
+    size_t value;
+    while(node->next != NULL){
+	prev = node;
+	node = node->next;
+    }
+
+    value = node->value;
+    prev->next = NULL;
+    free(node);
+
+    return value; 
+}
+
+void free_list(struct linked_list list) {
+    free_util(list.head);
+}
+
+void free_util(struct list_node* node){
+    if(node->next == NULL){
+	free(node);
+	return;
+    }
+
+    free_util(node->next);
+    free(node);
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,108 @@
 #include "queue.h"
 #include "tile_game.h"
+#include <stdlib.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+int checkWin(struct game_state* state);
+void printState(struct game_state* state);
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+void enqueue(struct queue *q, struct game_state state) {
+    insert_at_head(&q->data, serialize(state));
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+struct game_state dequeue(struct queue *q) {
+    if(q->data.head->next == NULL){
+	return deserialize(q->data.head->value);
+    }
+
+    return deserialize(remove_from_tail(&q->data)); 
+}
+
+int number_of_moves(struct game_state start) {
+    struct queue* q = malloc(sizeof(struct queue));
+
+    struct list_node* node = malloc(sizeof(struct list_node));
+    node->value = serialize(start);
+    node->next = NULL;
+    struct linked_list* list = malloc(sizeof(struct linked_list));
+    list->head = node;
+    q->data = *list;
+    struct game_state state; 
+    
+    int qSize = 1;
+    while(qSize > 0){
+	state = dequeue(q);
+	qSize--;
+    		
+	//Base case	
+	if(checkWin(&state) == 1){
+	    free_list(q->data);
+	    free(q);
+	    return state.num_steps;
+	}
+	
+	if(state.empty_row < 3){
+	    move_up(&state);
+	    enqueue(q, state);
+	    qSize++;
+	    move_down(&state);
+	    state.num_steps -= 2;
+	}
+	
+	if(state.empty_row > 0){
+	    move_down(&state);
+	    enqueue(q, state);
+	    qSize++;
+	    move_up(&state);
+	    state.num_steps-=2;
+	}
+
+	if(state.empty_col < 3){
+	    move_left(&state);
+	    enqueue(q, state);
+	    qSize++;
+	    move_right(&state);
+	    state.num_steps-=2;
+	}
+
+	if(state.empty_col > 0){
+	    move_right(&state);
+	    enqueue(q, state);
+	    qSize++;
+	    move_left(&state);
+	    state.num_steps-=2;
+	}
+
+    }
+    
+    
+    free_list(q->data);
+    free(q);
+
+    return 0;
+}
+
+// Checks if the game is finished
+int checkWin(struct game_state* state){
+    int r, c;
+    for(c = 0; c < 4; c++){
+	for(r = 0; r < 4; r++){
+	    if(r == 3 && c == 3) continue;
+	    if(state->tiles[r][c] != 4*r + c + 1){
+		return 0;
+	    }
+	}
+    }
+    return 1;
+}
+
+void printState(struct game_state* state){
+    printf("%d steps\n", state->num_steps);
+    int r, c;
+    for(r = 0; r < 4; r++){
+	for(c = 0; c < 4; c++){
+	    printf("%d ", state->tiles[r][c]);
+	}
+	printf("\n");
+    }
+    printf("\n");
+}
